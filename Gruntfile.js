f3ae40ee3ff2174d733f5fcae1205acd7396cc14
David Rimbault <rimbault.david@gmail.com>
2014-03-27 23:43:08 +0100

    Added some watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,21 @@ module.exports=function(grunt)
 {
   require('load-grunt-tasks')(grunt);
   grunt.initConfig({
-    
+    watch:{
+      css:{
+        files: ["**/*.css"],
+        options:{
+          spawn: false,
+          livereload: true
+        }
+      },
+      js:{
+        files:["**/*.js"],
+        option:{
+          spawn:false,
+          livereload: true
+        }
+      }
+    }
   });
 }
